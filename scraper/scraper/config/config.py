54e5f7115050d3e96d14f5b14ace58320b9edca5
Willy <wcr5048@psu.edu>
2015-10-03 18:27:58 -0400

    updated config

--- scraper/scraper/config/config.py
+++
@@ -1,3 +0,0 @@
-# defines entry urls for spiders
-CNN_FEED = ['http://rss.cnn.com/rss/cnn_topstories.rss']
-FOX_FEED = ['http://feeds.foxnews.com/foxnews/most-popular', 'http://feeds.foxnews.com/foxnews/latest', 'http://feeds.foxnews.com/foxnews/tech', 'http://feeds.foxnews.com/foxnews/national', 'http://feeds.foxnews.com/foxnews/world']
